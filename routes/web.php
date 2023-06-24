5516466c60e6e4433bc759117ae1fc4d504169da
Bradley Willies <bradley.willies@gmail.com>
2023-06-24 22:03:12 +0100

    added force https to web.php for production

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+if (env('APP_ENV') === 'production') {
+    \URL::forceScheme('https');
+}
+
 Route::get('/', function () {
     return view('index');
 });
